540d5082e5b457f2661e5954656ed4efdf2ffbff
VincenzoTavassi <info@egadivacanze.it>
2023-01-26 15:35:27 +0100

    logica + html completi

--- mail/js/main.js
+++ mail/js/main.js
@@ -2,6 +2,8 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull'esito del controllo.
 
+
+// ARRAY DI EMAIL 
 const listaEmail = [
     "info@ciao.com",
     "nomenome@ciaociao.com",
@@ -10,19 +12,36 @@ const listaEmail = [
     "test@test.org"
 ]
 
-const sceltaUtente = prompt('Scrivi la tua e-mail');
+// AUTORIZZAZIONE FALSA DI DEFAULT 
 let autorizzato = false;
 
-for (let i = 0; i < listaEmail.length; i++) {
-    let currentEmail = listaEmail[i];
+// EVENTO SUL CLICK DEL PULSANTE DI CONTROLLO AUTORIZZAZIONE
+document.getElementById('controlla').addEventListener('click', function () {
 
-    if (currentEmail == sceltaUtente) {
-        autorizzato = true;
+    // OTTENGO IL VALORE INSERITO DALL'UTENTE 
+    let sceltaUtente = document.getElementById('controllo-email').value;
+    // CICLO LA LISTA E-MAIL
+    for (let i = 0; i < listaEmail.length; i++) {
+        let currentEmail = listaEmail[i];
+        // SE C'E' UN'E-MAIL UGUALE A QUELLA DELL'UTENTE ALLORA E' AUTORIZZATO
+        if (currentEmail == sceltaUtente) {
+            autorizzato = true;
+        }
     }
-}
+    // MESSAGGIO DI AUTORIZZAZIONE 
+    if (autorizzato == true) {
+        alert(`Benvenuto ${sceltaUtente}`);
+    } else {
+        alert(`Non sei autorizzato`);
+    }
+})
 
-if (autorizzato == true) {
-    alert(`Benvenuto ${sceltaUtente}`);
-} else {
-    alert(`Non sei autorizzato`);
-}
+// EVENTO SUL CLICK DEL PULSANTE PER OTTENERE AUTORIZZAZIONE
+
+document.getElementById('autorizza').addEventListener('click', function () {
+    // OTTENGO IL VALORE INSERITO DALL'UTENTE 
+    let nuovoUtente = document.getElementById('email-da-autorizzare').value;
+    listaEmail.push(nuovoUtente);
+    alert(`Email ${nuovoUtente} inserita!`)
+
+})
