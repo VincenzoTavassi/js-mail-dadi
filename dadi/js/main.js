45e57a2f255bd24da789759788c5731aa38e1c3b
VincenzoTavassi <info@egadivacanze.it>
2023-01-26 15:18:53 +0100

    dadi full con html

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -2,17 +2,21 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-const numeroGiocatore = Math.floor(Math.random() * 6) + 1;
-console.log(numeroGiocatore);
-const numeroPc = Math.floor(Math.random() * 6) + 1;
-console.log(numeroPc);
 
-if (numeroGiocatore > numeroPc) {
-    alert('Hai vinto!')
-}
-else if (numeroGiocatore == numeroPc) {
-    alert('Pareggio!');
-}
-else {
-    alert('Hai perso!');
-}
+document.getElementById('genera-numero').addEventListener('click', function () {
+    const numeroGiocatore = Math.floor(Math.random() * 6) + 1;
+    const numeroPc = Math.floor(Math.random() * 6) + 1;
+
+    document.querySelector('#numero-giocatore').innerHTML = `Il tuo numero è ${numeroGiocatore}!`;
+    document.querySelector('#numero-pc').innerHTML = `Il numero del computer è ${numeroPc}!`;
+
+    if (numeroGiocatore > numeroPc) {
+        document.querySelector('#risultato').innerHTML = `<strong>HAI VINTO!</strong>`
+    }
+    else if (numeroGiocatore == numeroPc) {
+        document.querySelector('#risultato').innerHTML = `<strong>PAREGGIO!</strong>`
+    }
+    else {
+        document.querySelector('#risultato').innerHTML = `<strong>HAI PERSO!</strong>`
+    }
+})
