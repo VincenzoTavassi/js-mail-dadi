6278c3b89cd032619f701aff900705cf20cdbbd0
VincenzoTavassi <info@egadivacanze.it>
2023-01-26 15:21:03 +0100

    commenti

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -3,13 +3,15 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 
+// EVENTO SU CLICK DEL PULSANTE 
 document.getElementById('genera-numero').addEventListener('click', function () {
+    // GENERO NUMERI RANDOM 
     const numeroGiocatore = Math.floor(Math.random() * 6) + 1;
     const numeroPc = Math.floor(Math.random() * 6) + 1;
-
+    // MOSTRO I NUMERI ALL'UTENTE 
     document.querySelector('#numero-giocatore').innerHTML = `Il tuo numero è ${numeroGiocatore}!`;
     document.querySelector('#numero-pc').innerHTML = `Il numero del computer è ${numeroPc}!`;
-
+    // COMPARAZIONE DEI DUE NUMERI E RISULTATO 
     if (numeroGiocatore > numeroPc) {
         document.querySelector('#risultato').innerHTML = `<strong>HAI VINTO!</strong>`
     }
